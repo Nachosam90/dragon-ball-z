eb6fec355915dee1aa1d3107d837ba80fc4509f5
Nachosam <ignacio.sampedroolivera@gmail.com>
2024-08-20 17:06:29 +0200

    url actual

--- src/api/routes/characters.routes.js
+++ src/api/routes/characters.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const router = express.Router()
-const { getCharacters } = require("../controllers/characters.controllers");
+const router = express.Router();
+const { getCharacters, getCharacterById } = require("../controllers/characters.controllers")
 
-router.get("/characters", getCharacters)
+router.get("/characters", getCharacters);
+router.get("/characters/:id", getCharacterById)
 
-module.exports = router
+module.exports = router;
