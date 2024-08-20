288650be4978f600dcd4c3c9511149f5c827cd7f
Nachosam <ignacio.sampedroolivera@gmail.com>
2024-08-20 15:34:03 +0200

    cors

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const { connectDB } = require("./src/utils/db")
 const env = require("dotenv")
 env.config()
 const router = require("./src/api/routes/characters.routes")
+const cors = require("cors")
 
 
 connectDB();
 const server = express();
 const PORT = process.env.PORT; //usamos la variable de entorno PORT
-
+server.use(cors())
 server.use(express.json())
 server.use("/", router)
 
