0c31fc3396497108518254b03ba00bf2db2fd9c8
Nachosam <ignacio.sampedroolivera@gmail.com>
2024-08-20 14:51:37 +0200

    eliminar cloudinary

--- index.js
+++ index.js
@@ -4,16 +4,11 @@ const env = require("dotenv")
 env.config()
 const router = require("./src/api/routes/characters.routes")
 
-const cloudinary = require("cloudinary").v2
-cloudinary.config({
-    cloud_name: process.env.CLOUD_NAME,
-    api_key: process.env.CLOUD_API_KEY,
-    api_secret: process.env.CLOUD_API_SECRET
-})
 
 connectDB();
 const server = express();
 const PORT = process.env.PORT; //usamos la variable de entorno PORT
+
 server.use(express.json())
 server.use("/", router)
 
